7a44f819d0813ad95134b9c6099c4e70749106b1
rahul <rahul.valasai@gmail.com>
2022-06-27 09:40:11 +0530

    student grade.

---
+++ grade.js
@@ -0,0 +1,21 @@
+let mark = parseInt(prompt("Enter Your Mark"))
+
+if(mark >= 90)
+{
+    console.log("Student is A grade");
+}
+else if(mark >= 80)
+{
+    console.log("Student is B grade");
+}
+else if(mark >= 60)
+{
+    console.log("Student is C grade");
+}
+else if(mark >= 50)
+{
+    console.log("Student is D grade")
+}
+else{
+    console.log("Student is fail");
+}
